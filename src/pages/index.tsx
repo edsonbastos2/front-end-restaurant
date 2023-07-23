41401be99f604d8c7a0536cf1d18a799f6b22b25
Edson Bastos <edsonbastos2@hotmail.com>
2023-07-23 13:09:47 -0300

    criado tela de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,39 @@
 import Header from 'next/head'
+import styles from '../../styles/Home.module.scss'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/Button'
+
+
+
 export default function Home() {
   return (
     <>
       <Header>
         <title>Pizzaria nova - Faça seu login</title>
       </Header>
-      <div>
-        <h1>Sua <span>Logo</span></h1>
-        <Input
-          placeholder='Digite seu e-mail'
-          type='text'
-        />
-        <Input
-          placeholder='Digite sua senha'
-          type='password'
-        />
+      <div className={styles.container}>
+          <h1 className={styles.logo}>Sua<span>Logo</span></h1>
+        <div className={styles.login}>
 
-        <Button
-          type='submit'
-          loading={false}
-        >
-          Cadastrar
-        </Button>
+          <form>
+            <Input
+              placeholder='Digite seu e-mail'
+              type='text'
+            />
+            <Input
+              placeholder='Digite sua senha'
+              type='password'
+            />
+
+            <Button
+              type='submit'
+              loading={false}
+            >
+              Acessar
+            </Button>
+          </form>
+          <a className={styles.text}>Não tenho cadastro!Cadastre-se</a>
+        </div>
       </div>
     </>
   )
